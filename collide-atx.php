45f863f6f3aa3b331bfa58aa9bc694995e87af8f
pandabrand <pandabrand@gmail.com>
2017-02-03 14:11:53 -0600

    updated tracking ID

--- collide-atx.php
+++ collide-atx.php
@@ -87,6 +87,6 @@ function atx_post_type_queries($query) {
 }
 
 function add_google_analytics() {
-  echo "<script>window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','UA-52000426-2','auto');ga('send','pageview')</script><script src=\"https://www.google-analytics.com/analytics.js\" async defer></script>";
+  echo "<script>window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','UA-91408703-1','auto');ga('send','pageview')</script><script src=\"https://www.google-analytics.com/analytics.js\" async defer></script>";
 }
 add_action('wp_footer', 'add_google_analytics', 10);
